9ab3c0392d59415497c352596166f1fb9e3c0462
Marko Beelmann <marko.beelmann@gmail.com>
2019-01-30 23:38:25 +0100

    Inital commit

---
+++ Async-Await/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Async_Await
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MyDoAsync().Result);
+        }
+
+        static async Task<int> DoAsync()
+        {
+            await Task.Delay(10000).ConfigureAwait(false);
+            return 42;
+        }
+
+        static Task<int> MyDoAsync()
+        {
+            var stateMachine = new MyDoAsyncStateMachine();
+            stateMachine.methodBuilder = new AsyncTaskMethodBuilder<int>();
+            stateMachine.methodBuilder.Start(ref stateMachine);
+            return stateMachine.methodBuilder.Task;
+        }
+
+        public struct MyDoAsyncStateMachine : IAsyncStateMachine
+        {
+            public AsyncTaskMethodBuilder<int> methodBuilder;
+            public int state;
+            public TaskAwaiter awaiter;
+
+            public void MoveNext()
+            {
+                if (state == 0)
+                {
+                    awaiter = Task.Delay(10000).GetAwaiter();
+                    if (awaiter.IsCompleted)
+                    {
+                        state = 1;
+                        goto state1;
+                    }
+                    else
+                    {
+                        state = 1;
+                        methodBuilder.AwaitUnsafeOnCompleted(ref awaiter, ref this);
+                    }
+                    return;
+                }
+
+state1:
+                if(state == 1)
+                {
+                    awaiter.GetResult();
+                    methodBuilder.SetResult(42);
+                }
+            }
+
+            public void SetStateMachine(IAsyncStateMachine stateMachine)
+            {
+                methodBuilder.SetStateMachine(stateMachine);
+            }
+        }
+    }
+}
---
+++ CustomAwaiter/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public class Foo : INotifyCompletion
+    {
+        private Button _button;
+        public Foo(Button button) { _button = button; }
+        private Action _continuation;
+        public bool IsCompleted  => false;
+
+        // called when await is starting
+        public void OnCompleted(Action continuation)
+        {
+            _button.Click += OnButtonClick;
+            // cache the continuation
+            _continuation = continuation;
+            _button.Enabled = true;
+        }
+        // called when the button is clicked
+        private void OnButtonClick(object sender, EventArgs e)
+        {
+            _button.Click -= OnButtonClick;
+            _button.Text = "Done";
+            _button.Enabled = false;
+            // start the excution of the continuation after the await call
+            _continuation();
+        }
+
+        public void GetResult() { }
+
+    }
+
+
+    public static class Ext
+    {
+        /// Extension method that returns the awaiter when the button
+        /// is called with await
+        public static Foo GetAwaiter(this Button _btn)
+        {
+            // return a new instance of class foo which implements
+            // the INotifyCompletion interface
+            return new Foo(_btn);
+        }
+
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Setup();
+        }
+
+        private async void Setup()
+        {
+            button2.Enabled = false;
+            await button1;
+            await button2;
+            MessageBox.Show("All done");
+
+        }
+    }
+}
